efdc4143b9678d68fc23d775dffe0722c97b3d40
TabishAK <takchamp6@gmail.com>
2021-10-12 12:28:11 +0500

    Image issue

--- src/components/carousel/components/Simple.js
+++ src/components/carousel/components/Simple.js
@@ -9,8 +9,8 @@ import {
 import "pure-react-carousel/dist/react-carousel.es.css";
 import "./style.scss";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-import slide_1 from "../../../images/gallery/gallery_2.jpeg";
-import slide_2 from "../../../images/gallery/gallery_1.jpeg";
+import s1 from "../../../images/gallery/gallery_2.jpeg";
+import s2 from "../../../images/gallery/gallery_1.jpeg";
 
 export default class extends React.Component {
   render() {
@@ -25,7 +25,7 @@ export default class extends React.Component {
         <Slider>
           <Slide index={0}>
             <img
-              src={slide_1}
+              src={s1}
               alt="img-1"
               style={{
                 width: "100%",
@@ -35,7 +35,7 @@ export default class extends React.Component {
           </Slide>
           <Slide index={1}>
             <img
-              src={slide_2}
+              src={s2}
               alt="img-1"
               style={{
                 width: "100%",
@@ -45,7 +45,7 @@ export default class extends React.Component {
           </Slide>
           <Slide index={2}>
             <img
-              src={slide_2}
+              src={s2}
               alt="img-1"
               style={{
                 width: "100%",
